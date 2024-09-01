58e597d088dc6e35440cf977422683401ce0ff37
ankit10009 <ti.ankit1@gmail.com>
2024-09-01 19:40:03 +0530

    ECommerce Project From Telusko

---
+++ src/main/resources/data1.sql
@@ -0,0 +1,7 @@
+insert into product (name, desc, brand, category, price, release_Date, available, quantity) VALUES
+('Maruti Suzuki', 'features and performance', 'TaTa Motors','Cars', 750000.99, '2024-01-15', true, 45),
+('Mahindra Thar', 'fuel efficiency and reliable', 'Renault Motors','Cars' ,500000.00, '2018-02-01', false, 100),
+('Hyundai', 'fuel efficiency and reliable', 'Renault Motors','Cars', 700000.00, '2018-02-01', false, 100),
+('Tata Nexon', 'fuel efficiency and reliable', 'Renault Motors','Cars', 500000.00, '2018-02-01', false, 100),
+('Honda Accord', 'fuel efficiency and reliable', 'Renault Motors','Cars', 700000.00, '2018-02-01', false, 100);
+
